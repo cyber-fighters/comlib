69ea2eb65a2c75b2f3daa0d500bf4de2a6e235bd
Cajus Pollmeier <pollmeier@gonicus.de>
2019-01-07 08:30:25 +0100

    Use internal API

--- comlib/lib.py
+++ comlib/lib.py
@@ -1,15 +1,43 @@
+import os
+import requests
+
+
+class Unconfigured(Exception):
+    """Inidicate that the communication is unconfigured."""
+    pass
+
+
+class StatusFailed(Exception):
+    """Inidicate that the status communication failed."""
+    pass
+
+
 class BackyardCom:
     __id = ""
+    __base_url = None
 
     def __init__(self, id):
         self.__id = id
-        self.status(0, "initializing")
+
+        status_url = os.environ.get('STATUS_URL')
+        if status_url == None:
+            raise Unconfigured('No STATUS_URL environment variable found!')
+        
+        self.__base_url = os.path.join(status_url, self.__id)
+        self.status(0, 'initializing')
 
     def status(self, progress, message=""):
-        print("sending progress %d" % progress)
+        res = requests.patch(self.__base_url, data = {'id': self.__id, 'progress': progress, 'message': message})
+        if res.status_code != 200:
+            raise StatusFailed('Failed to update status: %s [%d]' % (res.status_code, res.text))
 
     def done(self, filename):
-        self.status(100, "uploading")
-        print("sending data to whatever")
-        self.status(100, "done")
+        self.status(100, 'uploading')
+
+        files = {'file': ('result.json', open(filename, 'rb'), 'application/json', {'Expires': '0'})}
+        res = requests.post(self.__base_url, files=files)
+        if res.status_code != 200:
+            raise StatusFailed('Failed to upload result: %s [%d]' % (res.status_code, res.text))
+
+        self.status(100, 'done')
 
